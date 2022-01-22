0a39e0912c2fb6b6ca38a656f38ec4b85260222b
wirdes <45904810+wirdes@users.noreply.github.com>
2022-01-22 10:18:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Store AP"),
+        ),
+        body: const Center(child: Text("Test App")),
+        drawer: const Drawer(),
+      ),
+    );
+  }
+}
